ddf23a6f0308a178dd7af261febc3be7a0f07a27
sumeyyesert <sumeyeesert@gmail.com>
2025-04-14 04:45:56 +0300

    Initial Commit

---
+++ ebookApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ebookApp
+//
+//  Created by Sümeyye Sert on 14.04.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
