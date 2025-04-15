3e3987d5c456ed5dda2b2544b9f66c665540b294
sumeyyesert <sumeyeesert@gmail.com>
2025-04-15 03:18:21 +0300

    initial commit

--- ebookApp/ViewController.swift
+++ ebookApp/ViewController.swift
@@ -11,7 +11,18 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        self.navigationItem.title = "E-Book"
+        
+        let appearance = UINavigationBarAppearance()
+        
+        appearance.backgroundColor = UIColor(named: "anaRenk")
+        appearance.titleTextAttributes = [.foregroundColor: UIColor(named: "yaziRenk1")!, .font: UIFont(name: "Lobster-Regular", size: 30)!]
+        
+        // bunlar yeni sürümle gelmiş, zorunlu, mantığı açıklanmadı
+        navigationController?.navigationBar.standardAppearance = appearance
+        navigationController?.navigationBar.compactAppearance = appearance
+        navigationController?.navigationBar.scrollEdgeAppearance = appearance
     }
 
 
